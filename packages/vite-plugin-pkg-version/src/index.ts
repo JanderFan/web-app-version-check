93e88fb1cc781a4d82b830abdce337d6ffb76367
fanjinde <2928528663@qq.com>
2025-12-11 13:12:55 +0800

    init

---
+++ packages/vite-plugin-pkg-version/src/index.ts
@@ -0,0 +1,37 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+import { cwd } from 'node:process';
+import type { Plugin } from 'vite';
+
+const pluginName = 'vite-plugin-version';
+let outDir = 'dist';
+let root = cwd();
+
+export function vitePluginVersion(): Plugin {
+  return {
+    name: pluginName,
+    apply: 'build',
+    configResolved(config) {
+      outDir = config.build.outDir;
+      root = config.root;
+    },
+
+    closeBundle(err) {
+      if (err) {
+        return;
+      }
+      const pkgFilePath = resolve(cwd(), 'package.json');
+      const pkgContent = readFileSync(pkgFilePath, 'utf-8');
+      const pkgJson = JSON.parse(pkgContent);
+      const version = pkgJson.version;
+      const versionFileContent = {
+        version: `v${version}`
+      };
+      const versionFilePath = resolve(root, outDir, 'version.json');
+
+      writeFileSync(versionFilePath, JSON.stringify(versionFileContent, null, 2), {
+        encoding: 'utf-8'
+      });
+    }
+  };
+}
---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  semi: true,
+  endOfLine: 'lf'
+};
